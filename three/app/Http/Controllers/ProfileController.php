c36f4a3b4cc862690427e3f8ff9ef59d631f7ffa
SamPM092 <sammich4294@gmail.com>
2024-10-04 17:34:11 +0700

    Calling database

--- three/app/Http/Controllers/ProfileController.php
+++ three/app/Http/Controllers/ProfileController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function profile() {
-        return view('profile');
+        // $book = new Book([
+        //     'cover' => 'one',
+        //     'title' => ' testing',
+        //     'author' => 'sam',
+        //     'desc' => 'something'
+        // ]);
+        // $book->save();
+        return view('profile', [
+            'books' => Book::all()
+        ]);
     }
 }
--- three/resources/views/profile.blade.php
+++ three/resources/views/profile.blade.php
@@ -6,11 +6,41 @@ BOOK LIST -->
         @section('content')
             <div class="row">
                 <div class="col-md-12">
-                    <div class="card  shadow-lg rounded">
-                        <div class="card-body">
-                            aa
+                <div class="card-body bg-light">
+                        <a href="#" class="btn btn-md btn-success mb-3" style="background: cadetblue">ADD BOOK</a>
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Book Cover</th>
+                                    <th scope="col">Book Title</th>
+                                    <th scope="col">Author</th>
+                                    <th scope="col">Date Added</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($books as $book)
+                                    <tr>
+                                        <td class="text-center">
+                                            <img src="" class="rounded" style="width: 150px">
+                                        </td>
+                                        <td>{{ $book->title }}</td>
+                                        <td>{{ $book->author }}</td>
+                                        <td>{{ $book->created_at }}</td>
+                                        <td class="text-center">
+                                            <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="" method="POST">
+                                                <a href="#" class="btn btn-sm btn-success">VIEW</a>  
+                                                <a href="#" class="btn btn-sm btn-primary">EDIT</a>
+
+                                                <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>  
+
                         </div>
-                    </div>
                 </div>
             </div>
         @endsection
