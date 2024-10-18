e6972971aeb1cc806ec607180361cb062dafa889
SamPM092 <sammich4294@gmail.com>
2024-10-18 20:31:40 +0700

    a little change for dashboard interface

--- three/resources/views/dashboard.blade.php
+++ three/resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.html')
 
-<body style="background-color:#130c0b;">
+<body style="background-color:#d9d9d9;">
     <style>
         @import "https://use.fontawesome.com/releases/v5.5.0/css/all.css";
 
@@ -44,8 +44,20 @@
             border-radius: .25rem;
             transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out, box-shadow .15s ease-in-out;
         }
+
+        .alt-card {
+            position: relative;
+            display: -ms-flexbox;
+            display: flex;
+            -ms-flex-direction: column;
+            flex-direction: column;
+            min-width: 0;
+            word-wrap: break-word;
+            background-color: #fff;
+            background-clip: border-box;
+        }
     </style>
-    <header id="header" class="p-3 text-white shadows" style="background-color: #130c0b;">
+    <header id="header" class="p-3 text-white shadows bg-dark">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
@@ -64,22 +76,23 @@
             </div>
         </div>
     </header>
-    <div>
+    <div style="margin: 40px">
         <form action="{{ route('bookView') }}" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" method="GET">
             <input name="search" type="search" class="form-control form-control-dark" placeholder="Search..."
                 aria-label="Search">
         </form>
     </div>
 
-    <div class="row" style="margin: auto;">
+    <div class="row justify-content-center" style="margin: 40px;">
         @foreach ($books as $book)
             <div>
-                <div class="card" style="background-color:transparent">
-                    <a class="border-curved" href="{{ route('detailed', $book->id) }}">
+                <div class="alt-card " style="background-color:transparent; margin: 30px">
+                    <a class="border-curved shadow" href="{{ route('detailed', $book->id) }}">
                         <div class="border-curved" style="width:250px; height: 325px;}">
                             <img class="book-cover" src="{{ Storage::url('public/books/' . $book->cover) }}" alt="">
                             <div class="judul_art">
-                                <p style="position: absolute;left: 1px; bottom: -15px; color: white; font-size: 20px;}">
+                                <p
+                                    style="position: absolute; bottom: -15px; color: white; font-size: 18px;margin:18px 12px">
                                     <b>{{ $book->title }}</b>
                                 <p>
                             </div>
