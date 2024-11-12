3ea9f1e9ccdd7644098fbb3e4614ccd8803d5c54
SamPM092 <sammich4294@gmail.com>
2024-11-12 17:18:40 +0700

    a little grouping

--- three/routes/web.php
+++ three/routes/web.php
@@ -23,13 +23,16 @@ Route::get('/regist', [IndexController::class, 'regist'])->name('regist');
 Route::post('/regist', [IndexController::class, 'registore'])->name('registore');
 Route::get('/about', [ProfileController::class, 'about'])->name('about');
 Route::get('/dashboard', [ProfileController::class, 'bookView'])->name('bookView');
-Route::get('/profile', [ProfileController::class, 'profile'])->name('profile'); //name digunakan pada saat memanggil rute di html
 Route::get('/create', [ProfileController::class, 'createV'])->name('create'); //create book
 Route::get('{book}/edit', [ProfileController::class, 'edit'])->name('edit'); // edit book
 Route::get('{book}/detailed', [ProfileController::class, 'detailed'])->name('detailed');
-Route::post('/profile', [ProfileController::class, 'store'])->name('store');
 Route::put('{book}/', [ProfileController::class, 'update'])->name('update');
-Route::delete('/profile/{id}', [ProfileController::class, 'destroy'])->name('delete'); //delete book
-Route::put('{/profile', [ProfileController::class, 'updateProfile'])->name('updateProfile');
-Route::delete('/profile/', [ProfileController::class, 'destroyProfile'])->name('destroyProfile');
-Route::get('/about', [IndexController::class,'about'])->name('about');
+Route::get('/profile', [ProfileController::class, 'profile'])->name('profile'); //name digunakan pada saat memanggil rute di html
+
+Route::group(['prefix' => 'profile/'], function(){ //Prefix untuk URL
+    Route::post('', [ProfileController::class, 'store'])->name('store');
+    Route::delete('/{id}', [ProfileController::class, 'destroy'])->name('delete'); //delete book
+    Route::put('{', [ProfileController::class, 'updateProfile'])->name('updateProfile');
+    Route::delete('/', [ProfileController::class, 'destroyProfile'])->name('destroyProfile');
+});
+Route::get('/about', [IndexController::class, 'about'])->name('about');
