5c5f83cf3598fdf841395664d4e670942624d534
SamPM092 <sammich4294@gmail.com>
2025-03-03 15:47:26 +0700

    basic authorization

--- forapi/routes/api.php
+++ forapi/routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'v1', 'namespace' => ''], function(){
+Route::group(['prefix' => 'v1', 'namespace' => '', 'middleware' => 'auth:sanctum'], function(){
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('invoices', InvoiceController::class);
 
