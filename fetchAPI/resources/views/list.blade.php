734225a8e9c24483a0c93455fd8f03af65100475
SamPM092 <sammich4294@gmail.com>
2025-03-26 12:40:09 +0700

    done, option and showing its content

--- fetchAPI/resources/views/list.blade.php
+++ fetchAPI/resources/views/list.blade.php
@@ -8,12 +8,43 @@
 </head>
 
 <body>
-    <select id="cars">
-            @foreach($customers['data'] as $book)
-            <option value="{{ $book['title'] }}">{{ $book['title'] }}</option>
-            @endforeach
-        </select>
+    <select id="book">
+        <option value="">-- Select a book --</option>
+        @foreach($customers['data'] as $book)
+            <option value="{{ $book['id'] }}" 
+            data-title="{{ $book['title'] }}" 
+            data-author="{{ $book['author'] }}"
+            data-genre="{{ $book['genre'] }}" 
+            data-description="{{ $book['description'] }}">
+                {{ $book['title'] }}
+            </option>
+        @endforeach
+    </select>
+    <div id="book-details" style="display: none;">
+        <h1 id="book-title"></h1>
+        <p id="book-author"></p>
+        <p><strong>Genre:</strong> <span id="book-genre"></span></p>
+        <p id="book-description"></p>
+    </div>
 
+    <script>
+        document.getElementById("book").addEventListener("change", function () {
+            let selectedOption = this.options[this.selectedIndex];
+
+            if (selectedOption.value) {
+                // Get book details from data attributes
+                document.getElementById("book-title").textContent = selectedOption.getAttribute("data-title");
+                document.getElementById("book-author").textContent = "Author: " + selectedOption.getAttribute("data-author");
+                document.getElementById("book-genre").textContent = selectedOption.getAttribute("data-genre");
+                document.getElementById("book-description").textContent = selectedOption.getAttribute("data-description");
+
+                // Show the details section
+                document.getElementById("book-details").style.display = "block";
+            } else {
+                document.getElementById("book-details").style.display = "none";
+            }
+        });
+    </script>
 </body>
 
 </html>
